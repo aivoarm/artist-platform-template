9af1b8d5d6d3f3a18fecad07bd3c396340614163
Arman Ayva <aayvazy@gmail.com>
2025-11-09 21:08:26 -0500

    Fresh start and initial project commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+// tailwind.config.js
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  // ... other configuration like theme, plugins, etc.
+}
